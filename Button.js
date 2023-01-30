9313466073d34bb455bfc73da91afb62f30dd9f6
elisetchan <chan.el@northeastern.edu>
2023-01-30 16:08:06 -0500

    updated js, working now

--- Button.js
+++ Button.js
@@ -1,7 +1,8 @@
 let count = 0;
 
 function inc() {
-    let text = document.getElementByID("content");
+    let text = document.getElementById("content");
     count = count + 1;
-    text.innerHTML = "Number of Times Button Clicked: ";
+    text.innerHTML = "Number of Times Button Clicked: " + count;
 }
+
